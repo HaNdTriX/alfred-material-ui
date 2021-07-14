d1fb8d3c8aff5646971a2352f4c7d3df6193dfe6
Henrik Wenz <henrik.wenz@gmail.com>
2021-07-14 10:54:23 +0200

    Add userConfig

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const alfy = require('alfy')
+const alfy = require("alfy");
 
-const APPLICATION_ID = 'bh4d9od16a'
-const API_KEY = '1d8534f83b9b0cfea8f16498d19fbcab'
-const INDEX_NAME = 'material-ui'
-const URL = `https://${APPLICATION_ID}-dsn.algolia.net/1/indexes/*/queries?x-algolia-application-id=${APPLICATION_ID.toUpperCase()}&x-algolia-api-key=${API_KEY}`
+const APPLICATION_ID = "bh4d9od16a";
+const API_KEY = "1d8534f83b9b0cfea8f16498d19fbcab";
+const URL = `https://${APPLICATION_ID}-dsn.algolia.net/1/indexes/*/queries?x-algolia-application-id=${APPLICATION_ID.toUpperCase()}&x-algolia-api-key=${API_KEY}`;
 
-alfy
-  .fetch(URL, {
-    method: 'POST',
-    body: {
-      requests: [{
-        indexName: INDEX_NAME,
-        params: `query=${alfy.input}&hitsPerPage=6`
-      }]
-    }
-  })
-  .then(({ results }) => {
-    const { hits } = results[0]
-    const items = hits.map(result => ({
-      title: Object.values(result.hierarchy)
-        .filter(Boolean)
-        .join(' > '),
-      subtitle: result._snippetResult.content.value
-        .replace(/<[^>]+>/g, '')
-        .replace(/\n/g, ' ')
-        .replace(/\s+/g, ' ') + '...',
-      arg: result.url,
-      quicklookurl: result.url,
-      mods: {
-        alt: {
-          subtitle: result.anchor
-        }
-      }
-    }))
+const indexName = "material-ui";
+const hitsPerPage = alfy.userConfig.get("hitsPerPage");
+const language = alfy.userConfig.get("language");
+const branch = alfy.userConfig.get("branch");
+const query = alfy.input;
 
-    alfy.output(items)
-  })
-  .catch(alfy.error)
+const data = await alfy.fetch(URL, {
+  method: "POST",
+  body: {
+    requests: [
+      {
+        indexName,
+        params: new URLSearchParams({
+          query,
+          hitsPerPage,
+          facetFilters: `["version:${branch}","language:${language}"]`,
+        }).toString(),
+      },
+    ],
+  },
+});
+
+const items = data.results[0].hits.map((result) => ({
+  title: Object.values(result.hierarchy).filter(Boolean).join(" > "),
+  subtitle:
+    result._snippetResult.content.value
+      .replace(/<[^>]+>/g, "")
+      .replace(/\n/g, " ")
+      .replace(/\s+/g, " ") + "...",
+  arg: result.url,
+}));
+
+alfy.output(items);
