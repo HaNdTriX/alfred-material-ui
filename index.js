30695f66833b1d86bdb2419fbf82738ea92df3e0
Henrik Wenz <henrik.wenz@gmail.com>
2022-03-18 10:59:49 +0100

    v1.0.0

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const alfy = require("alfy");
+import alfy from "alfy";
 
 const APPLICATION_ID = "bh4d9od16a";
 const API_KEY = "1d8534f83b9b0cfea8f16498d19fbcab";
-const url = `https://${APPLICATION_ID}-dsn.algolia.net/1/indexes/*/queries?x-algolia-application-id=${APPLICATION_ID.toUpperCase()}&x-algolia-api-key=${API_KEY}`;
+const url = `https://${APPLICATION_ID}-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.10.5)%3B%20Browser%20(lite)%3B%20docsearch%20(3.0.0)%3B%20docsearch-react%20(3.0.0)&x-algolia-api-key=${API_KEY}&x-algolia-application-id=${APPLICATION_ID.toUpperCase()}`;
 const indexName = "material-ui";
 const hitsPerPage = alfy.userConfig.get("hitsPerPage");
 const language = alfy.userConfig.get("language");
@@ -15,12 +15,12 @@ const facetFilters = JSON.stringify([
 
 const items = await alfy.fetch(url, {
   method: "POST",
-  body: {
+  json: {
     requests: [
       {
+        query,
         indexName,
         params: new URLSearchParams({
-          query,
           hitsPerPage,
           facetFilters,
         }).toString(),
@@ -28,15 +28,14 @@ const items = await alfy.fetch(url, {
     ],
   },
   transform: ({ results }) =>
-    results[0].hits.map((result) => ({
-      title: Object.values(result.hierarchy).filter(Boolean).join(" > "),
-      subtitle:
-        result._snippetResult.content.value
-          .replace(/<[^>]+>/g, "")
-          .replace(/\n/g, " ")
-          .replace(/\s+/g, " ") + "...",
-      arg: result.url,
-    })),
+    results[0].hits.map((hit) => {
+      const { lvl0, ...other } = hit.hierarchy;
+      return {
+        title: Object.values(other).filter(Boolean).join(" > "),
+        subtitle: lvl0,
+        arg: hit.url,
+      };
+    }),
 });
 
 alfy.output(items);
